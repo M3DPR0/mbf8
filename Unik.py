796b3ae5362471bb340ca96810a54ad7e21f3eda
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-13 20:26:59 +0700

    Update Unik.py

--- Unik.py
+++ Unik.py
@@ -89,30 +89,30 @@ def menu(n,toket):
 	elif unikers in ["1"]:
 		print("\n%s[*]%s from : %s"%(P,W,n))
     
-		for z in s.get(url.format("me/friends?access_token=%s"%(toket,timeout=None))).json()["data"]:
+		for z in s.get(url.format("me/friends?access_token=%s"%(toket))).json()["data"]:
 			target.append(z["id"])
       
       
 	elif unikers in ["2"]:
 		try:
 			idf = input("\n%s[*] %sID friend : "%(P,W))
-			k = s.get(url.format(idf+"?access_token=%s"%(toket,timeout=None))).json()["name"]
+			k = s.get(url.format(idf+"?access_token=%s"%(toket))).json()["name"]
 		except KeyError:
 			exit("%s[!]%s ups sorry friend not found !!"%(R,W))
 		print("%s[*]%s from : %s"%(P,W,k))
     
-		for f in s.get(url.format(idf+"/friends?access_token=%s"%(toket,timeout=None))).json()["data"]:
+		for f in s.get(url.format(idf+"/friends?access_token=%s"%(toket))).json()["data"]:
 			target.append(f["id"])
       
       
 	elif unikers in ["3"]:
 		try:	
 			idg = input("\n%s[*]%s ID group : "%(P,W))
-			e = s.get(url.format("group/?id="+idg+"&access_token=%s"%(toket,timeout=None))).json()["name"]
+			e = s.get(url.format("group/?id="+idg+"&access_token=%s"%(toket))).json()["name"]
 		except KeyError:
 			exit("%s[!]%s ups sorry group not found !!"%(R,W))
 		print("%s[*]%s from : %s"%(P,W,e))
-		for y in s.get(url.format(idg+"/members?fields=name,id&limit=4999&access_token=%s"%(toket,timeout=None))).json()["data"]:
+		for y in s.get(url.format(idg+"/members?fields=name,id&limit=4999&access_token=%s"%(toket))).json()["data"]:
 			target.append(y["id"])
       
       
